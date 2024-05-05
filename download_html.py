529c96070fbd09820cb92f66fbc0c15464b5519e
David M Walker <davidw@datamgmt.com>
2024-05-05 14:47:03 +0100

    Update

--- download_html.py
+++ download_html.py
@@ -1,30 +1,41 @@
+"""
+
+download_html.py by David M Walker
+
+A utility to download the weekly chart data from 14-Nov-1952 to date and store 
+them for later processing. If a .html file already exists then no attempt is made
+to download it. 
+
+"""
+
 import datetime
 from datetime import date
 import os
 import requests
 
 def download_file(url):
-   local_filename = f"{output_dir}{url.strip('/').split('/')[-1]}.html"
-   if not os.path.exists(local_filename):
-      with requests.get(url, stream=True) as r:
-         r.raise_for_status()
-         with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-               if chunk:
-                  f.write(chunk)
-   return local_filename
+    """ Function to download the file for a given week """
+    local_filename = f"{OUTPUT_DIR}{url.strip('/').split('/')[-1]}.html"
+    if not os.path.exists(local_filename):
+        with requests.get(url, stream=True, timeout=30) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+    return local_filename
 
 today = date.today()
 fetch_date = datetime.date(1952,11,14)
-output_dir = "data/html/"
+OUTPUT_DIR = "data/html/"
 
-if not os.path.exists(output_dir):
-   os.makedirs(output_dir)
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
 while fetch_date < today:
-   page = f"https://www.officialcharts.com/charts/singles-chart/{fetch_date.strftime('%Y%m%d')}"
-   print (page)
-   download_file(page)
-   fetch_date = fetch_date + datetime.timedelta(7)
+    page = f"https://www.officialcharts.com/charts/singles-chart/{fetch_date.strftime('%Y%m%d')}"
+    print (page)
+    download_file(page)
+    fetch_date = fetch_date + datetime.timedelta(7)
 
 print ("Finished")
--- scrape_to_csv.py
+++ scrape_to_csv.py
@@ -1,75 +1,102 @@
-from bs4 import BeautifulSoup
+"""
+
+scrape_to_csv.py by David M Walker
+
+A utility to scrape the weekly chart data from the files downloaded by download_html.py
+Each .html file is read and an equivalent .csv file is created. A file called all_data.csv
+is created that has the data from all the source files in a single file.
+
+"""
+
+# pylint: disable=C0301
+
 import glob
 import csv
-import base64
 import os
+from bs4 import BeautifulSoup
 
-output_dir = "data/csv/"
-input_files = "data/html/*.html"
+OUTPUT_DIR = "data/csv/"
+INPUT_FILES = "data/html/*.html"
 
-if not os.path.exists(output_dir):
-   os.makedirs(output_dir)
-   
-all_data_file = output_dir + "all_data.csv"
-   
-with open(all_data_file, 'w', newline='') as all_csvfile:
-   
-   fieldnames = ['chart_date', 'chart_position', 'chart_artist', 'chart_title', 'chart_movement', 'chart_peak', 'chart_weeks']
-   all_writer = csv.DictWriter(all_csvfile, fieldnames=fieldnames, lineterminator=os.linesep)
-   all_writer.writeheader()
-    
-   for path in sorted(glob.glob(input_files)):
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
-     print(path)
-       
-     this_data_file = output_dir + path.split('/')[-1].split('.')[0] + ".csv"
+ALL_DATA_FILE = OUTPUT_DIR + "all_data.csv"
 
-     with open(this_data_file, 'w', newline='') as this_csvfile:
-      
-        this_writer = csv.DictWriter(this_csvfile, fieldnames=fieldnames, lineterminator=os.linesep)
-        this_writer.writeheader()
+with open(ALL_DATA_FILE, 'w', newline='', encoding="utf-8") as all_csvfile:
 
-        with open(path, "r") as file:
-            
-           soup = BeautifulSoup(file.read(), 'html.parser')
-            
-           chart_date  = soup.select("div.prose span.sr-only")[0].text.split(" on ")[1]
-            
-           rows = [row for row in soup.select("div.chart-item") ]
+    fieldnames = ['chart_date', 'CHART_POSITION', 'CHART_ARTIST',
+                 'CHART_TITLE', 'CHART_MOVEMENT', 'CHART_PEAK', 'CHART_WEEKS']
+    all_writer = csv.DictWriter(all_csvfile, fieldnames=fieldnames, lineterminator=os.linesep)
+    all_writer.writeheader()
 
-           for row in rows:
-               
-              chart_position = ""
-              chart_artist = ""
-              chart_title = ""
-              chart_movement = "New"
-              chart_peak = ""
-              chart_weeks = ""
-                
-              if len(row.select("div.chart-item-content div.position strong") ):
-                 chart_position = row.select("div.chart-item-content div.position strong")[0].text
-               
-              if len(row.select("a.chart-artist span")):
-                 chart_artist = row.select("a.chart-artist span")[0].text
+    for path in sorted(glob.glob(INPUT_FILES)):
 
-              if len(row.select("a.chart-name span")):
-                 # Specifically to deal with 14 Nv 1952 - first ever chart
-                 if len(row.select("a.chart-name span"))==1:
-                    chart_title = row.select("a.chart-name span")[0].text 
-                 if len(row.select("a.chart-name span"))!=1:
-                    chart_title = row.select("a.chart-name span")[1].text 
-                    
-              if len(row.select("li.movement span")):
-                 chart_movement = row.select("li.movement span")[1].text
+        print(path)
 
-              if len(row.select("li.peak span")):
-                 chart_peak = row.select("li.peak span")[0].text
+        this_data_file = OUTPUT_DIR + path.split('/')[-1].split('.')[0] + ".csv"
 
-              if len(row.select("li.weeks span")):
-                 chart_weeks = row.select("li.weeks span")[0].text
-                  
-              if len(chart_position):
-                 all_writer.writerow({'chart_date': chart_date, 'chart_position': chart_position, 'chart_artist': chart_artist, 'chart_title': chart_title, 'chart_movement': chart_movement, 'chart_peak': chart_peak, 'chart_weeks': chart_weeks})
-                 this_writer.writerow({'chart_date': chart_date, 'chart_position': chart_position, 'chart_artist': chart_artist, 'chart_title': chart_title, 'chart_movement': chart_movement, 'chart_peak': chart_peak, 'chart_weeks': chart_weeks})
+        with open(this_data_file, 'w', newline='', encoding="utf-8") as this_csvfile:
+
+            this_writer = csv.DictWriter(this_csvfile, fieldnames=fieldnames,
+                lineterminator=os.linesep)
+            this_writer.writeheader()
+
+            with open(path, "r", encoding="utf-8") as file:
+
+                soup = BeautifulSoup(file.read(), 'html.parser')
+
+                chart_date_data = soup.select("div.prose span.sr-only")[0].text.split(" on ")[1]
+                chart_date = f"{int(chart_date_data.split('/')[2]):04d}-{int(chart_date_data.split('/')[1]):02d}-{int(chart_date_data.split('/')[0]):02d}"
+
+                #rows = [row for row in soup.select("div.chart-item") ]
+                rows = list(soup.select("div.chart-item"))
+
+                for row in rows:
+
+                    CHART_POSITION = ""
+                    CHART_ARTIST = ""
+                    CHART_TITLE = ""
+                    CHART_MOVEMENT = "New"
+                    CHART_PEAK = ""
+                    CHART_WEEKS = ""
+
+                    if len(row.select("div.chart-item-content div.position strong") ):
+                        CHART_POSITION = row.select("div.chart-item-content div.position strong")[0].text
+
+                    if len(row.select("a.chart-artist span")):
+                        CHART_ARTIST = row.select("a.chart-artist span")[0].text
+
+                    if len(row.select("a.chart-name span")):
+                    # Specifically to deal with 14 Nv 1952 - first ever chart
+                        if len(row.select("a.chart-name span"))==1:
+                            CHART_TITLE = row.select("a.chart-name span")[0].text
+                        else:
+                            CHART_TITLE = row.select("a.chart-name span")[1].text
+
+                    if len(row.select("li.movement span")):
+                        CHART_MOVEMENT = row.select("li.movement span")[1].text
+
+                    if len(row.select("li.peak span")):
+                        CHART_PEAK = row.select("li.peak span")[0].text
+
+                    if len(row.select("li.weeks span")):
+                        CHART_WEEKS = row.select("li.weeks span")[0].text
+
+                    if len(CHART_POSITION) > 0:
+                        all_writer.writerow({'chart_date': chart_date,
+                                             'CHART_POSITION': CHART_POSITION,
+                                             'CHART_ARTIST': CHART_ARTIST,
+                                             'CHART_TITLE': CHART_TITLE, 
+                                             'CHART_MOVEMENT': CHART_MOVEMENT,
+                                             'CHART_PEAK': CHART_PEAK, 
+                                             'CHART_WEEKS': CHART_WEEKS})
+                        this_writer.writerow({'chart_date': chart_date,
+                                              'CHART_POSITION': CHART_POSITION,
+                                              'CHART_ARTIST': CHART_ARTIST, 
+                                              'CHART_TITLE': CHART_TITLE, 
+                                              'CHART_MOVEMENT': CHART_MOVEMENT, 
+                                              'CHART_PEAK': CHART_PEAK, 
+                                              'CHART_WEEKS': CHART_WEEKS})
 
 print ("Finished")
