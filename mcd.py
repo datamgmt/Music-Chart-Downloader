b75c08451c463f0ad94b1cd9479cefe40da2a955
David M Walker <davidw@datamgmt.com>
2024-05-13 15:34:49 +0100

    Updated with autopep8

--- mcd.py
+++ mcd.py
@@ -18,6 +18,7 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
 def csv_writer(filename, content):
     """
     Write output to a csv file
@@ -27,7 +28,10 @@ def csv_writer(filename, content):
                   "chart_title", "chart_movement", "chart_peak", "chart_weeks"]
 
     with open(filename, "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator=os.linesep)
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames=fieldnames,
+            lineterminator=os.linesep)
         writer.writeheader()
         writer.writerows(content)
 
@@ -41,7 +45,8 @@ def date_validator(check_date, chart):
 
     chart_day = chart_data[chart]["day"]
     first_chart_date = chart_data[chart]["first"]
-    last_chart_date = today - datetime.timedelta( (7 - chart_day + today.weekday()) % 7)
+    last_chart_date = today - \
+        datetime.timedelta((7 - chart_day + today.weekday()) % 7)
 
     # Try to convert to a date object
     try:
@@ -50,7 +55,8 @@ def date_validator(check_date, chart):
         sys.exit("Incorrect data format, should be YYYYMMDD")
 
     # Move to the last chart day
-    check_date = check_date - datetime.timedelta( (7 - chart_day + check_date.weekday()) % 7)
+    check_date = check_date - \
+        datetime.timedelta((7 - chart_day + check_date.weekday()) % 7)
     check_date = check_date.date()
 
     # Set minimum date to first date for chart
@@ -63,8 +69,8 @@ def date_validator(check_date, chart):
 
 
 def download_file(url, local_filename):
-    """ 
-    Function to download the file for a given week 
+    """
+    Function to download the file for a given week
     """
 
     if not os.path.exists(local_filename):
@@ -94,21 +100,25 @@ def process_files_for_date(working_date):
     """
 
     date_string = working_date.strftime("%Y%m%d")
-    print (f"Processing: {args.chart}-{date_string}")
+    print(f"Processing: {args.chart}-{date_string}")
 
     page = f"{args.chart_url_prefix}/{date_string}"
     local_html = f"{args.datadir}/html/{args.chart}-{date_string}.html"
     download_file(page, local_html)
 
     with open(local_html, "r", encoding="utf-8") as file:
-        file_content=process_html_file(file, working_date, args.chart)
+        file_content = process_html_file(file, working_date, args.chart)
         if "all" in args.output_set:
             all_content.extend(file_content)
         if "weekly" in args.output_set:
             if "csv" in args.output_type:
-                csv_writer(f"{args.datadir}/csv/{args.chart}-{date_string}.csv", file_content)
+                csv_writer(
+                    f"{args.datadir}/csv/{args.chart}-{date_string}.csv",
+                    file_content)
             if "json" in args.output_type:
-                json_writer(f"{args.datadir}/json/{args.chart}-{date_string}.json", file_content)
+                json_writer(
+                    f"{args.datadir}/json/{args.chart}-{date_string}.json",
+                    file_content)
 
 
 def process_html_file(filehandle, chart_date, chart):
@@ -126,22 +136,22 @@ def process_html_file(filehandle, chart_date, chart):
         rows = list(soup.select("div.chart-item"))
 
         for row in rows:
-            entry = process_officialcharts_entry(row,chart_date)
+            entry = process_officialcharts_entry(row, chart_date)
             if entry["chart_position"]:
                 chart_entries.append(entry)
 
     return chart_entries
 
 
-def process_officialcharts_entry(rowdata,entry_date):
+def process_officialcharts_entry(rowdata, entry_date):
     """
     Parse each entry for a UK Singles Chart format file
     """
 
-    entry = {"chart_date":datetime.datetime.strftime(entry_date, "%Y%m%d"),
-             "chart_movement":"New"}
+    entry = {"chart_date": datetime.datetime.strftime(entry_date, "%Y%m%d"),
+             "chart_movement": "New"}
 
-    if len(rowdata.select("div.chart-item-content div.position strong") ):
+    if len(rowdata.select("div.chart-item-content div.position strong")):
         entry["chart_position"] = rowdata.select("div.position strong")[0].text
     else:
         entry["chart_position"] = 0
@@ -152,7 +162,7 @@ def process_officialcharts_entry(rowdata,entry_date):
 
     if len(rowdata.select("a.chart-name span")):
         # Specifically deal the first chart where there is no movement
-        if len(rowdata.select("a.chart-name span"))==1:
+        if len(rowdata.select("a.chart-name span")) == 1:
             entry["chart_title"] = rowdata.select("a.chart-name span")[0].text
         else:
             entry["chart_title"] = rowdata.select("a.chart-name span")[1].text
@@ -174,15 +184,16 @@ def validate_args(arglist):
     Validate all passed parameters
     """
 
-    arglist.startdate=date_validator(arglist.startdate, arglist.chart)
-    arglist.enddate=date_validator(arglist.enddate, arglist.chart)
+    arglist.startdate = date_validator(arglist.startdate, arglist.chart)
+    arglist.enddate = date_validator(arglist.enddate, arglist.chart)
 
     if arglist.startdate > arglist.enddate:
-        print (f"Warning: Start Date ({arglist.startdate}) is after End Date ({arglist.enddate})")
-        print ("         Automatically transposing start and end dates")
-        tmp_date=arglist.startdate
-        arglist.startdate=arglist.enddate
-        arglist.enddate=tmp_date
+        print(
+            f"Warning: Start Date ({arglist.startdate}) is after End Date ({arglist.enddate})")
+        print("         Automatically transposing start and end dates")
+        tmp_date = arglist.startdate
+        arglist.startdate = arglist.enddate
+        arglist.enddate = tmp_date
 
     folder_list = ["html"]
     folder_list.extend(arglist.output_type)
@@ -191,7 +202,8 @@ def validate_args(arglist):
             try:
                 os.makedirs(f"{arglist.datadir}/{folder}")
             except OSError:
-                sys.exit(f"Failed to make directory {arglist.datadir}/{folder}")
+                sys.exit(
+                    f"Failed to make directory {arglist.datadir}/{folder}")
 
     arglist.chart_url_prefix = chart_data[arglist.chart]["url"]
 
@@ -203,43 +215,52 @@ def validate_args(arglist):
 parser = argparse.ArgumentParser(
     description="The Music Chart Data Collector",
     epilog="(c)2024 Data Management & Warehousing ")
-parser.add_argument("--chart",
-    choices=["uk-singles", "uk-albums"],
+parser.add_argument(
+    "--chart",
+    choices=[
+        "uk-singles",
+        "uk-albums"],
     default="uk-singles",
     help="Which music chart to download, (Default: %(default)s)")
-parser.add_argument("--startdate",
+parser.add_argument(
+    "--startdate",
     default="19521114",
     help="The first chart to download in YYYYMMDD format (Default: %(default)s)")
-parser.add_argument("--enddate",
+parser.add_argument(
+    "--enddate",
     default=datetime.date.today().strftime("%Y%m%d"),
     help="The last chart to download in YYYYMMDD format (Default: %(default)s)")
-parser.add_argument("--datadir",
+parser.add_argument(
+    "--datadir",
     default="./data",
     help="Location of datafiles used in processing (Default: %(default)s)")
 parser.add_argument("--output_type",
+                    nargs="*",
+                    choices=["csv", "json"],
+                    default=["csv"],
+                    help="Output file formats required (Default: %(default)s)")
+parser.add_argument(
+    "--output_set",
     nargs="*",
-    choices=["csv", "json"],
-    default=["csv"],
-    help="Output file formats required (Default: %(default)s)")
-parser.add_argument("--output_set",
-    nargs="*",
-    choices=["weekly", "all"],
+    choices=[
+        "weekly",
+        "all"],
     default=["all"],
     help="Weekly charts and/or one large file (Default: %(default)s)")
 
 # url: URL of the historical charts
 # day: Day of week chart released (0=Monday ... 6=Sunday)
 # first: Dates of the very first chart for each organisation
-chart_data = {"uk-singles": {"url":"https://www.officialcharts.com/charts/singles-chart/",
-                             "day":4, 
-                             "first":datetime.date(1952, 11, 14)},
-              "uk-albums": {"url":"https://www.officialcharts.com/charts/albums-chart/", 
-                            "day":4, 
-                            "first":datetime.date(1956, 7, 28)},
-              "billboard-top-100": {"url":"undefined",
-                                    "day":6, 
-                                    "first":datetime.date(1958, 8, 4)}
-             }
+chart_data = {"uk-singles": {"url": "https://www.officialcharts.com/charts/singles-chart/",
+                             "day": 4,
+                             "first": datetime.date(1952, 11, 14)},
+              "uk-albums": {"url": "https://www.officialcharts.com/charts/albums-chart/",
+                            "day": 4,
+                            "first": datetime.date(1956, 7, 28)},
+              "billboard-top-100": {"url": "undefined",
+                                    "day": 6,
+                                    "first": datetime.date(1958, 8, 4)}
+              }
 
 args = validate_args(parser.parse_args())
 
@@ -262,4 +283,4 @@ if "all" in args.output_set:
     if "json" in args.output_type:
         json_writer(f"{args.datadir}/json/{args.chart}-all.json", all_content)
 
-print ("Finished")
+print("Finished")
